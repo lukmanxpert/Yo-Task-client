f004f17138fa10a29cb4c23b3966a55feb8184ab
lukmanxpert <lukmanmiiah2004@gmail.com>
2025-02-27 20:17:26 +0600

    update todo list

--- src/categories/Todo.jsx
+++ src/categories/Todo.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import TaskEdidModal from "../modal/TaskEdidModal";
-import { FaEye } from 'react-icons/fa6';
 import TaskDelete from "../modal/TaskDelete";
-import { GiClick } from 'react-icons/gi';
 import axios from "axios";
 const Todo = ({ filteredTask, refetch }) => {
     const { title, description, date, category, _id } = filteredTask
@@ -17,45 +15,36 @@ const Todo = ({ filteredTask, refetch }) => {
     function closeModal() {
         setIsOpen(false)
     }
-
     const taskDelete = () => {
         axios.delete(`http://localhost:5000/task/${_id}`)
             .then(result => {
                 console.log(result.data);
                 refetch()
-                // setIsEditModalOpen(false)
             })
     }
-
-
     return (
-        <div className='border bg-[#00000054] mb-2 p-2 rounded-sm' >
+        <div className='border bg-[#bdc3c7] text-white mb-2 p-2 rounded-sm' >
             <div className="space-y-2 ">
-                <h1 className="text-xl font-bold">{title}</h1>
-                <p className="text-gray-800">{description}</p>
+                <h1 className="text-xl text-black font-bold">{title}</h1>
+                <p className="text-black">{description}</p>
                 <div className="flex items-end gap-0">
-                    <h1 className="bg-red-500 font-bold py-1 px-4 rounded-full">{category}</h1>
-                    <h3 className="py-1 px-4">{formattedDate}</h3>
+                    <h1 className="bg-pink-700 text-white font-bold py-1 px-4 rounded-full">{category}</h1>
+                    <h3 className="py-1 text-black px-4">{formattedDate}</h3>
                 </div>
             </div>
-
-            {/* */}
             <div className="flex items-center justify-between">
-                <div className=' py-5  text-sm'>
+                <div className='py-5 text-sm'>
                     <span
                         onClick={() => { setIsEditModalOpen(true) }}
                         className='relative cursor-pointer inline-block px-3 py-2 font-semibold text-white leading-tight'
                     >
-
                         <span
                             aria-hidden='true'
                             className='absolute inset-0  bg-black black rounded-full'
                         ></span>
                         <div className='flex items-center gap-2'>
-                            <span className='relative '>Update</span>
-                            <FaEye className=' relative text-lg' />
+                            <span className='relative'>Update</span>
                         </div>
-
                     </span>
                     <TaskEdidModal
                         isOpen={isEditModalOpen}
@@ -64,7 +53,6 @@ const Todo = ({ filteredTask, refetch }) => {
                         refetch={refetch}
                     />
                 </div>
-                {/* delete */}
                 <div className=''>
                     <div className='flex justify-end' >
                         <span
@@ -75,10 +63,8 @@ const Todo = ({ filteredTask, refetch }) => {
                                 aria-hidden='true'
                                 className='absolute inset-0 bg-black  rounded-full'
                             ></span>
-
                             <div className='flex items-center gap-0'>
                                 <span className='relative'>Delete</span>
-                                <GiClick className=' relative text-lg' />
                             </div>
                         </span>
                     </div>
@@ -86,7 +72,6 @@ const Todo = ({ filteredTask, refetch }) => {
                 </div>
             </div>
         </div>
-
     );
 };
 
